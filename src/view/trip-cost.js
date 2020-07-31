df03941549b0b0a0387532b5b688542454d433aa
Stanislaw Chirko <post.chirko@gmail.com>
2020-07-31 13:52:09 +0200

    добавляет выполнение задания 5. Личный проект: время разбивать камни

---
+++ src/view/trip-cost.js
@@ -0,0 +1,8 @@
+// Информация о стоимости
+export const createSiteTripCostTemplate = () => {
+  return (
+    `<p class="trip-info__cost">
+      Total: &euro;&nbsp;<span class="trip-info__cost-value">1230</span>
+    </p>`
+  );
+};
---
+++ src/view/trip-events-list.js
@@ -0,0 +1,7 @@
+// Список событий поездки
+export const createSiteTripEventsListTemplate = () => {
+  return (
+    `<ul class="trip-events__list">
+    </ul>`
+  );
+};
